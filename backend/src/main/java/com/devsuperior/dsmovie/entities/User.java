fa3651a73b2e5287e8601ad3216549e503dddf0a
timoteo5@icloud.com <timoteodp10@outlook.com>
2022-01-12 23:08:32 -0300

    Find movies

--- backend/src/main/java/com/devsuperior/dsmovie/entities/User.java
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/User.java
@@ -9,12 +9,10 @@ import javax.persistence.Table;
 @Entity
 @Table(name= "tb_user")
 public class User {
-	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String email;
-	
 	public User() {
 		
 	}
